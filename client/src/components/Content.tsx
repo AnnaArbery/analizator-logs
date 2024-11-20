95976c72e91bb33e8fffe3f8114e4abc05ab78c9
Anna Arbery <anna.arbery@gmail.com>
2024-11-20 20:26:28 +0300

    simple version search-fileter

--- client/src/components/Content.tsx
+++ client/src/components/Content.tsx
@@ -1,48 +1,167 @@
 // import { Table } from 'antd';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Table from './Table/Table.tsx';
 // import columns from './Table/columns.ts';
 import logServerType from '../types/logServerType.ts';
-import { Card, Button } from 'antd';
+import { Card, Button, Input, Space } from 'antd';
 // import { Select, Input, Segmented, Space } from 'antd';
-import type { TableColumnsType } from 'antd';
+import type { InputRef, TableColumnsType } from 'antd';
+import Highlighter from 'react-highlight-words';
 
 type ContentProps = {
   list: logServerType[];
   columnsTitlesTable: TableColumnsType<logServerType>;
 };
 
-const columnsTitlesExtends: TableColumnsType<logServerType> = [
-  {
-    key: 'date',
-    render: (date: number) =>
-      `${new Intl.DateTimeFormat('ru-RU', {
-        timeStyle: 'short',
-        dateStyle: 'short',
-      }).format(date)}`,
-  },
-  {
-    key: 'status',
-    onFilter: (value, record) => {
-      if (value === 'Другие') return ![200, 404, 301].includes(record.status);
-      return record.status === value;
-    },
-  },
-  {
-    key: 'method',
-    onFilter: (value, record) => record.method === value,
-  },
-  {
-    dataIndex: 'agent',
-    key: 'agent',
-    onFilter: (value: any, record: logServerType) =>
-      record.agent.includes(value),
-  },
-];
+type DataIndex = keyof logServerType;
 
 const Content = ({ list, columnsTitlesTable }: ContentProps) => {
   let columns = columnsTitlesTable;
+
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef<InputRef>(null);
+
+  const handleSearch = (
+    selectedKeys: string[],
+    confirm: FilterDropdownProps['confirm'],
+    dataIndex: DataIndex
+  ) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters: () => void) => {
+    clearFilters();
+    setSearchText('');
+  };
+
+  const getColumnSearchProps = (
+    dataIndex: DataIndex
+  ): TableColumnType<DataType> => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+      close,
+    }) => (
+      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() =>
+            handleSearch(selectedKeys as string[], confirm, dataIndex)
+          }
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type='primary'
+            onClick={() =>
+              handleSearch(selectedKeys as string[], confirm, dataIndex)
+            }
+            // icon={<SearchOutlined />}
+            size='small'
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size='small'
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+          {/* <Button
+            type='link'
+            size='small'
+            onClick={() => {
+              confirm({ closeDropdown: false });
+              setSearchText((selectedKeys as string[])[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button> */}
+          <Button
+            type='link'
+            size='small'
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    // filterIcon: (filtered: boolean) => (
+    //   <SearchOutlined style={{ color: filtered ? '#1677ff' : undefined }} />
+    // ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes((value as string).toLowerCase()),
+    // filterDropdownProps: {
+    //   onOpenChange(open) {
+    //     if (open) {
+    //       setTimeout(() => searchInput.current?.select(), 100);
+    //     }
+    //   },
+    // },
+    // render: (text: string) =>
+    //   searchedColumn === dataIndex ? (
+    //     <Highlighter
+    //       highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+    //       searchWords={[searchText]}
+    //       autoEscape
+    //       textToHighlight={text ? text.toString() : ''}
+    //     />
+    //   ) : (
+    //     text
+    //   ),
+  });
+
+  const columnsTitlesExtends: TableColumnsType<logServerType> = [
+    {
+      key: 'ip',
+      ...getColumnSearchProps('ip'),
+    },
+    {
+      key: 'date',
+      render: (date: number) =>
+        `${new Intl.DateTimeFormat('ru-RU', {
+          timeStyle: 'short',
+          dateStyle: 'short',
+        }).format(date)}`,
+    },
+    {
+      key: 'status',
+      onFilter: (value, record) => {
+        if (value === 'Другие') return ![200, 404, 301].includes(record.status);
+        return record.status === value;
+      },
+    },
+    {
+      key: 'method',
+      onFilter: (value, record) => record.method === value,
+    },
+    {
+      dataIndex: 'agent',
+      key: 'agent',
+      onFilter: (value: any, record: logServerType) =>
+        record.agent.includes(value),
+    },
+  ];
 
   columnsTitlesExtends.forEach((item) => {
     let idxInitCol = columns.findIndex((col) => col.key === item.key);
