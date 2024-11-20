846036c3ccb24a3c488d59b21c4467c4c441bf13
Anna Arbery <anna.arbery@gmail.com>
2024-11-20 23:40:36 +0300

    fix bug pagination

--- client/src/components/Table/Table.tsx
+++ client/src/components/Table/Table.tsx
@@ -1,6 +1,7 @@
 import { Table as TableAnt } from 'antd';
 import type { TableColumnsType } from 'antd';
 import logServerType from '../../types/logServerType';
+import { useState } from 'react';
 
 type TableProps = {
   data: logServerType[];
@@ -8,6 +9,7 @@ type TableProps = {
 };
 
 const Table = ({ data, columns }: TableProps) => {
+  const [count, setCount] = useState(50);
   // @ts-ignore
   const onChange = (pagination, filters, sorter, extra) => {
     console.log('params', pagination, filters, sorter, extra);
@@ -17,7 +19,11 @@ const Table = ({ data, columns }: TableProps) => {
     <TableAnt<logServerType>
       dataSource={data}
       columns={columns}
-      pagination={{ pageSize: 50 }}
+      pagination={{
+        pageSizeOptions: ['30', '50', '100'],
+        pageSize: count,
+        onChange: (page, pageSize) => setCount(pageSize),
+      }}
       scroll={{ y: 55 * 12 }}
       bordered
       onChange={onChange}
